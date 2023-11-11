98e1cb4383623e21b23b9e99503586ba6734cea7
sunnybhandal <sunnybhandal123@gmail.com>
2023-11-11 14:58:10 -0700

    okay now navbar is good

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,38 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NAV_LINKS } from "../constants";
 import Link from "next/link";
 
 const Navbar = () => {
   const [openMenuModal, setOpenMenuModal] = useState(false);
-  console.log(openMenuModal);
+  const [direction, setDirection] = useState("up");
+  let oldScrollY = 0;
+
+  const controlDirection = () => {
+    if (window.scrollY !== 0) {
+      setDirection("down");
+    } else {
+      setDirection("up");
+    }
+    oldScrollY = window.scrollY;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlDirection);
+    return () => {
+      window.removeEventListener("scroll", controlDirection);
+    };
+  }, []);
 
   return (
-    <div className="padding-container py-2 border-b-2 border-[#116D99]">
+    <div
+      className={`padding-container bg-main fixed py-2 w-screen z-20 ${
+        direction == "down"
+          ? `transition-all transform border-b-2 border-[#116D99]`
+          : `border-none`
+      }`}
+    >
       <div className="flex justify-between">
         <Image
           src="/logo.svg"
@@ -25,7 +48,7 @@ const Navbar = () => {
             <Link
               href={link.href}
               key={link.key}
-              className="regular-18 cursor-pointer border-b-2 border-[#efefef] hover:border-b-2  hover:border-[#89CFF0]"
+              className="medium-18 cursor-pointer border-b-2 border-transparent hover:border-b-2  hover:border-[#89CFF0]"
             >
               {link.label}
             </Link>
@@ -45,14 +68,14 @@ const Navbar = () => {
 
         {/* Mobile Menu */}
         {openMenuModal && (
-          <div className="absolute z-20 bg-[#efefef] top-0 left-0 border-2 border-red-500 flex flex-row justify-between w-full h-full">
+          <div className="absolute z-20 bg-[#efefef] top-0 left-0 flex flex-row justify-between w-full h-screen">
             <div className="pl-6 pt-4">
               {NAV_LINKS.map((link) => (
                 <div className="flex gap-8">
                   <Link
                     href={link.href}
                     key={link.key}
-                    className="medium-18 mb-2 cursor-pointer border-b-2 border-[#efefef] hover:border-b-2 hover:border-[#89CFF0]"
+                    className="medium-18 mb-2 cursor-pointer border-b-2 border-transparent hover:border-b-2 hover:border-[#89CFF0]"
                   >
                     {link.label}
                   </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,44 @@
 import Image from "next/image";
-import { BIO } from "./constants";
+import { BIO, LINKS } from "./constants";
 import Button from "./components/Button";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="border-2 border-red-500">
-      <div className="max-container px-6 lg:px-32 py-14">
-        <div className="flex flex-col lg:flex-row gap-8 lg:gap-20 items-center">
-          <div className="flex flex-col gap-0 items-center">
-            <Image
-              src="headshot.svg"
-              alt="headshot"
-              width={250}
-              height={100}
-              className="w-40 lg:w-60"
-            />
-            <h4 className="bold-18 mt-2">Sunny Bhandal</h4>
-            <p>UX Designer & Software Developer</p>
-            <div className="flex gap-8 mt-2">
-              <Button type="button" title="LinkedIn" variant="btn_primary" />
-              <Button type="button" title="Figma" variant="btn_secondary" />
-            </div>
+    <div className="max-container px-6 lg:px-32 pt-20">
+      <div className="flex flex-col lg:flex-row gap-8 lg:gap-20 items-center">
+        <div className="flex flex-col gap-0 items-center">
+          <Image
+            src="headshot.svg"
+            alt="headshot"
+            width={250}
+            height={100}
+            className="w-40 lg:w-60"
+          />
+          <h4 className="bold-22 mt-2">Sunny Bhandal</h4>
+          <p className="mt-2">UX Designer & Software Developer</p>
+          <div className="flex gap-8 mt-3">
+            {LINKS.map((link) => (
+              <Link
+                href={link.href}
+                key={link.label}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <Button
+                  type="button"
+                  title={link.label}
+                  variant={link.variant}
+                />
+              </Link>
+            ))}
           </div>
-          <div className="flex flex-col gap-4">
-            <p className="">{BIO.p1}</p>
-            <p className="">{BIO.p2}</p>
-            <p className="">{BIO.p3}</p>
-            <p className="medium-16">{BIO.quote}</p>
-          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          <p className="">{BIO.p1}</p>
+          <p className="">{BIO.p2}</p>
+          <p className="">{BIO.p3}</p>
+          <p className="medium-16">{BIO.quote}</p>
         </div>
       </div>
     </div>
