49a1c1bdf1649864264abfa5652be6ab323db772
sunnybhandal <sunnybhandal123@gmail.com>
2023-11-16 09:06:29 -0700

    one last width fix

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -22,7 +22,7 @@ const Modal = ({ isOpen, onClose, title, content, href }: ModalProps) => {
       ></div>
 
       {/* Modal */}
-      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-main px-4 md:px-6 py-2 md:py-4 w-[355px] md:w-[500px] rounded-lg shadow-md z-10 border-4 border-[#89CFF0]">
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-main px-4 md:px-6 py-2 md:py-4 w-[363px] md:w-[500px] rounded-lg shadow-md z-10 border-4 border-[#89CFF0]">
         <div className="flex justify-between mb-4">
           <h4 className="semiBold-20 md:semiBold-22">{title}</h4>
           <Image
